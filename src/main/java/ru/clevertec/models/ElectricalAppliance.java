8e694e2d941f287bafad70b79f4f393a179c70e2
pashpashovich <pavel.kosovich2004@gmail.com>
2024-08-06 19:01:49 +0300

    Added class ElectricalAppliance

---
+++ src/main/java/ru/clevertec/models/ElectricalAppliance.java
@@ -0,0 +1,40 @@
+package ru.clevertec.models;
+
+import java.io.Serializable;
+
+public abstract class ElectricalAppliance implements Serializable {
+    private final String name;
+    private final double power;
+
+    public ElectricalAppliance(String  name, double power) {
+        this.name = name;
+        this.power = power;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPower() {
+        return power;
+    }
+
+    public abstract void turnOn();
+    public abstract void turnOff();
+
+    @Override
+    public String toString() {
+
+        return "Электроприбор: " + name + ", Мощность: " + power + " ватт";
+    }
+    @Override
+    public boolean equals(Object obj) {
+        final ElectricalAppliance other = (ElectricalAppliance) obj;
+        if ((obj == null) || (getClass() != obj.getClass()) || (!this.name.equals(other.name))) {
+            return false;
+        }
+        return this.power == other.power;
+    }
+}
+
+
